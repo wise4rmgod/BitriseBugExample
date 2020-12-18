90fffc262d379138041acddb9ccc1738b58392c7
Nwokocha wisdom maduabuchi <wise4rmgod@gmail.com>
2020-12-18 13:59:06 +0100

    add UI test

--- app/src/test/java/com/dev/bitrisebugexample/ValidateEmailTest.kt
+++ app/src/test/java/com/dev/bitrisebugexample/ValidateEmailTest.kt
@@ -12,7 +12,7 @@ class ValidateEmailTest() {
     @Test
     fun checkForSymbol() {
 
-        val result = Validate.checkEmail("wise@")
+        val result = Validate.checkEmail("wise@t")
         assertThat(result).contains("@")
     }
 
