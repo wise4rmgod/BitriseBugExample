8fbeae254f3bc0185960f7e5d61f2158a6266549
Nwokocha wisdom maduabuchi <wise4rmgod@gmail.com>
2021-03-12 22:18:35 +0100

    testing

--- app/src/main/java/com/dev/bitrisebugexample/MainActivity.kt
+++ app/src/main/java/com/dev/bitrisebugexample/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    lateinit var john: String
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
