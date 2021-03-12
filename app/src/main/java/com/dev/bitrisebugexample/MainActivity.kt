2c34cf3eaaea9e78e681192f174da9d4d1b7dbcc
Nwokocha wisdom maduabuchi <wise4rmgod@gmail.com>
2021-03-12 22:59:45 +0100

    testing

--- app/src/main/java/com/dev/bitrisebugexample/MainActivity.kt
+++ app/src/main/java/com/dev/bitrisebugexample/MainActivity.kt
@@ -15,9 +15,9 @@ class MainActivity : AppCompatActivity() {
             Validate.checkLengthOfWord(description.text.toString())
         }
 // Snippet-1
-        john.filter{ it.isLetter()}
-            .map{ it }
-            .filter{ it > 10.toChar() }
+        john.filter{it.isLetter()}
+            .map{it}
+            .filter{it > 10.toChar()}
             .min()
 
     }
